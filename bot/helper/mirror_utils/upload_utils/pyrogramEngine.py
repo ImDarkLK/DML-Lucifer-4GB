1fe47e672434558cc5dba61cd01a15b86962d874
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-06-14 13:17:37 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -62,14 +62,26 @@ class TgUploader:
         self.__listener.onUploadComplete(None, size, self.__msgs_dict, self.__total_files, self.__corrupted, self.name)
 
     def __upload_file(self, up_path, file_, dirpath):
+        besic=file_
+        if len(file_)>60:	
+         ext=file_.split('.')[-1]	
+         file_='.'.join(file_.split('.')[:-1])	
+         file_=file_.replace('_','.')	
+         if len(file_)>(59-len(ext)):	
+                file_=file_[:(59-len(ext))]	
+         file_=file_+'.'+ext	
+        print('saef ' +file_)	
+        new_path = ospath.join(dirpath, file_)	
+        osrename(up_path, new_path)	
+        up_path = new_path
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"{CUSTOM_FILENAME} <code>{file_}</code>"
+            cap_mono = f"{CUSTOM_FILENAME} <b>{file_}\n\n┏━━━━•❅•°•❈•°•❅•━━━━┓\n👑ᴍᴏᷱᴠͤɪᴇ ᴄʟͣᴜͬʙͤ ғᐃᴍɪʟʏ👑✰\n┗━━━━•❅•°•❈•°•❅•━━━━┛\n🎭Proudly Presented By🎭\n@MovieClubFamily</b>"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
             up_path = new_path
         else:
-            cap_mono = f"<code>{file_}</code>"
+            cap_mono = f"<b>{besic}\n\n┏━━━━•❅•°•❈•°•❅•━━━━┓\n👑ᴍᴏᷱᴠͤɪᴇ ᴄʟͣᴜͬʙͤ ғᐃᴍɪʟʏ👑✰\n┗━━━━•❅•°•❈•°•❅•━━━━┛\n🎭Proudly Presented By🎭\n@MovieClubFamily</b>"
         notMedia = False
         thumb = self.__thumb
         self.__is_corrupted = False

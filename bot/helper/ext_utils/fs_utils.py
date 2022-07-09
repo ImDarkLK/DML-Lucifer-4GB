1ec299b3aeee44b0f4a27337a3aa39e4ef58168c
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-07-09 10:06:48 +0530

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -124,8 +124,8 @@ def split_file(path, size, file_, dirpath, split_size, start_time=0, i=1, inLoop
             srun(["new-api", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
                   "-i", path, "-fs", str(split_size), "-map", "0", "-map_chapters", "-1", "-c", "copy", out_path])
             out_size = get_path_size(out_path)
-            if out_size > 4244635700:
-                dif = out_size - 4244635700
+            if out_size > 2097152000:
+                dif = out_size - 2097152000
                 split_size = split_size - dif + 5000000
                 osremove(out_path)
                 return split_file(path, size, file_, dirpath, split_size, start_time, i, True)

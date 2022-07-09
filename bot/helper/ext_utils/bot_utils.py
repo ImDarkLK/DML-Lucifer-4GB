4b2bdb64f49b8c83a1e6de691150e912c6c0547e
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-07-09 09:14:06 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -57,8 +57,8 @@ def get_readable_file_size(size_in_bytes) -> str:
     if size_in_bytes is None:
         return '0B'
     index = 0
-    while size_in_bytes >= 2048:
-        size_in_bytes /= 2048
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
         index += 1
     try:
         return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
